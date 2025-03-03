f558b573a005679f6ab3d9005e7b411712bd553c
Jose Silva <127362636+JoseSilva2004@users.noreply.github.com>
2025-03-02 22:01:31 -0400

    mejora en como se muestra la informacion

--- inventory/src/lib.rs
+++ inventory/src/lib.rs
@@ -1,3 +1,4 @@
+use std::io::{self};
 pub struct Item {
     id: u32,
     codigo_inventario: String,
@@ -20,36 +21,65 @@ impl Inventory {
         self.items.push(Item { id, codigo_inventario, name, model, price, quantity });
     }
 
-    pub fn remove_item(&mut self, id: u32) {
-        self.items.retain(|item| item.id != id);
+    //Eliminar un calzado por Codigo de inventario
+    pub fn eliminar_calzado<F>(&mut self, codigo_inventario: &str, clear_screen: F) 
+    where
+        F: Fn(),
+    {
+        clear_screen();
+        let initial_count = self.items.len();
+        self.items.retain(|item| item.codigo_inventario != codigo_inventario);
         
-        if self.items.iter().find(|item| item.id == id).is_none() {
-            println!("El ítem con ID {} fue eliminado correctamente.", id);
+        if self.items.len() < initial_count {
+            println!("El Calzado con código {} fue eliminado correctamente.", codigo_inventario);
         } else {
-            println!("El ítem con ID {} no fue encontrado.", id);
+            println!("El Calzado con código {} no fue encontrado.", codigo_inventario);
         }
+
+        wait_for_keypress();
     }
 
     /*Eliminar inventario por ID */
-    pub fn remove_inventory(&mut self, id: u32, quantity: u32) {
-        let item = self.items.iter_mut().find(|item| item.id == id);
+    pub fn remove_inventory<F>(&mut self, codigo_inventario: &str, quantity: u32, clear_screen: F)
+    where
+        F: Fn(),
+    {
+        clear_screen();
+        let item = self.items.iter_mut().find(|item| item.codigo_inventario == codigo_inventario);
         if let Some(item) = item {
             if item.quantity >= quantity {
                 item.quantity -= quantity;
-                println!("Se han eliminado {} pares del calzado con ID {}.", quantity, id);
+                println!("Se han eliminado {} pares del calzado con Referencia {}.", quantity, codigo_inventario);
             } else {
-                println!("No hay suficientes pares del calzado con ID {} en el inventario.", id);
+                println!("No hay suficientes pares del calzado con Referencia {} en el inventario.", codigo_inventario);
             }
         } else {
-            println!("El calzado con ID {} no fue encontrado.", id);
+            println!("El calzado con Referencia {} no fue encontrado.", codigo_inventario);
         }
+
+        wait_for_keypress();
+
     }
 
-    pub fn list_items(&self) {
+    pub fn list_items<F>(&self, clear_screen: F)
+    where
+        F: Fn(), // F es un closure que no toma argumentos y no devuelve nada
+    {
+        clear_screen(); // Limpiar la pantalla usando el callback
+        println!("\n{:<5} {:<15} {:<15} {:<15} {:<10} {:<10}", "ID", "Código", "Marca", "Modelo", "Precio", "Cantidad");
+        println!("{}", "-".repeat(70));
+        
         for item in &self.items {
-            println!("------------------------------------");
-            println!("ID: {}, \nCodigo: {} \nMarca: {}, \nModelo: {}, \nPrecio: {:.2} $, \nCantidad en inventario: {}", item.id, item.codigo_inventario, item.name, item.model, item.price, item.quantity);
-            println!("------------------------------------");
+            println!("{:<5} {:<15} {:<15} {:<15} {:<10.2} {:<10}", item.id, item.codigo_inventario, item.name, item.model, item.price, item.quantity);
         }
+        
+        wait_for_keypress();
     }
+
+}
+
+pub fn wait_for_keypress() {
+    println!("\nPresione Enter para continuar...");
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).expect("Error al leer la entrada");
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,20 @@
 //Sistema de inventario para el área de calzado de la Mr Price de Costa Azul
-use  inventory::Inventory;
+use inventory::{wait_for_keypress, Inventory};
 use std::io;
+use std::process::Command;
+
+pub fn clear_screen() {
+    if cfg!(target_os = "windows") {
+        Command::new("cmd")
+            .args(&["/C", "cls"])
+            .status()
+            .expect("Fallo al ejecutar el comando");
+    } else {
+        Command::new("clear")
+            .status()
+            .expect("Fallo al ejecutar el comando");
+    }
+}
 
 fn main() {
     let mut inventory = Inventory::new();
@@ -10,7 +24,7 @@ fn main() {
         println!("\nOpciones:");
         println!("1. Añadir Calzado");
         println!("2. Eliminar Calzado");
-        println!("3. Eliminar inventario por ID");
+        println!("3. Eliminar inventario por Referencia");
         println!("4. Listar todos los calzados");
         println!("5. Salir del programa");
 
@@ -19,6 +33,7 @@ fn main() {
         
         match choice.trim() {
             /*Añadir un calzado */
+
             "1" => {
                 let mut id = String::new();
                 let mut codigo_inventario = String::new();
@@ -35,7 +50,7 @@ fn main() {
                 
                 println!("\nIngrese la marca:");
                 io::stdin().read_line(&mut name).expect("Error al leer la entrada");
-
+                
                 println!("\nIngrese el modelo:");
                 io::stdin().read_line(&mut model).expect("Error al leer la entrada");
 
@@ -44,41 +59,45 @@ fn main() {
                 
                 println!("\nIngrese la cantidad en el inventario:");
                 io::stdin().read_line(&mut quantity).expect("Error al leer la entrada");
+
+                clear_screen();
                 
                 /*Se hace la conversion a su tipo de datos correspondiente */
                 let id: u32 = id.trim().parse().expect("Por favor, ingrese un número válido"); 
                 let quantity: u32 = quantity.trim().parse().expect("Por favor, ingrese un número válido");
                 let price : f32 = price.trim().parse().expect("Por favor, ingrese un número válido");
 
-                inventory.add_item(id, codigo_inventario.trim().to_string(), name.trim().to_string(), model.trim().to_string(), price, quantity);
+                inventory.add_item(id, codigo_inventario.trim().to_string().to_uppercase(), name.trim().to_string().to_uppercase(), model.trim().to_string().to_uppercase(), price, quantity);
                 println!("\n¡Calzado añadido correctamente!.");
+                wait_for_keypress();
+                
             }
             /*Eliminar un calzado */
             "2" => {
-                let mut id = String::new();
-                println!("Ingrese ID del ítem a eliminar:");
-                io::stdin().read_line(&mut id).expect("Error al leer la entrada");
+                let mut codigo_inventario = String::new();
+                println!("Ingrese codigo de referencia del calzado a eliminar:");
+                io::stdin().read_line(&mut codigo_inventario).expect("Error al leer la entrada");
                 
-                let id: u32 = id.trim().parse().expect("Por favor, ingrese un número válido");
-                inventory.remove_item(id);
+                inventory.eliminar_calzado(&codigo_inventario.trim().to_uppercase(), clear_screen);
             }
-            /*Eliminar inventario de un calzado por ID */
+            /*Eliminar inventario de un calzado por Referencia */
             "3" => {
-                let mut id = String::new();
+                let mut codigo_inventario = String::new();
                 let mut quantity = String::new();
-                println!("Ingrese el ID del calzado:");
-                io::stdin().read_line(&mut id).expect("Error al leer la entrada");
+                println!("Ingrese el código de referencia del calzado:");
+                io::stdin().read_line(&mut codigo_inventario).expect("Error al leer la entrada");
                 
                 println!("Ingrese la cantidad a eliminar:");
                 io::stdin().read_line(&mut quantity).expect("Error al leer la entrada");
                 
-                let id: u32 = id.trim().parse().expect("Por favor, ingrese un número válido");
                 let quantity: u32 = quantity.trim().parse().expect("Por favor, ingrese un número válido");
-                inventory.remove_inventory(id, quantity);
-            }
-            /*Listar todos los calzados */
-            "4" => inventory.list_items(),
+                inventory.remove_inventory(&codigo_inventario.trim().to_uppercase(), quantity, clear_screen);
 
+            }
+            
+            /*Listar todos los calzados */
+            "4" => inventory.list_items(clear_screen),
+            
             /*Salir del programa */
             "5" => break,
             _ => println!("Opción no válida, por favor intente de nuevo."),
